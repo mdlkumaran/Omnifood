05dc40dadd8bc5614d95f4ceb417dca7f749fa07
Kumaran MDL <mdlkumaran@gmail.com>
2018-08-08 07:18:49 -0700

    map integration and responsive

--- resources/js/script.js
+++ resources/js/script.js
@@ -7,8 +7,8 @@ $(document).ready(function () {
             $('nav').removeClass('sticky');
         }
     }, {
-            offset: '60px;'
-        });
+        offset: '60px;'
+    });
 
 
     /* scroll to buttons */
@@ -64,41 +64,60 @@ $(document).ready(function () {
     $('.js--wp-1').waypoint(function () {
         $('.js--wp-1').addClass('animated fadeIn');
     }, {
-            offset: '50%'
-        });
+        offset: '50%'
+    });
 
     $('.js--wp-2').waypoint(function () {
         $('.js--wp-2').addClass('animated fadeInUp');
     }, {
-            offset: '50%'
-        });
+        offset: '50%'
+    });
 
     $('.js--wp-3').waypoint(function () {
         $('.js--wp-3').addClass('animated fadeIn');
     }, {
-            offset: '50%'
-        });
+        offset: '50%'
+    });
 
     $('.js--wp-4').waypoint(function () {
         $('.js--wp-4').addClass('animated pulse');
     }, {
-            offset: '50%'
-        });
-});
+        offset: '50%'
+    });
 
-/*mobile nav */
+    /* mobile nav */
 
-$('.js--nav-icon').click(function () {
-    var nav = $('.js--main-nav');
-    var icon = $('.js--nav-icon i');
+    $('.js--nav-icon').click(function () {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
 
-    nav.slideToggle(200);
+        nav.slideToggle(200);
 
-    if(icon.hasClass('ion-navicon-round')) {
-        icon.addClass('ion-close-round');
-        icon.removeClass('ion-navicon-round')
-    } else {
-        icon.addClass('ion-navicon-round');
-        icon.removeClass('ion-close-round')
-    }
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round')
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round')
+        }
+    });
+
+    /* maps */
+
+    var map = new GMaps({
+        div: '.map',
+        lat: 38.7437395,
+        lng: -9.19,
+        zoom: 12
+    });
+
+    map.addMarker({
+        lat: 38.7437395,
+        lng: -9.2304147,
+        title: 'Lisbon',
+        infoWindow: {
+            content: '<p>Our Lisbon Headquarters</p>'
+        }
+    });
+
 });
